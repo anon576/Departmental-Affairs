7d3d7be28839db4795dd290a5eb2bd07e2a43c30
Abhishek Bhoyar <abhibhoyar141@gmail.com>
2024-10-01 12:02:49 +0530

    conferance done

---
+++ server/Handler/AuthHandler.mjs
@@ -0,0 +1,173 @@
+import JwtOperation from '../Utils/jwtoken.mjs'
+import { pool } from '../Database/database.mjs'
+import { generateOTP, sendOTPMail } from '../Utils/email.mjs'
+
+
+class AuthHandler {
+
+    static adminLogin = async (req, res) => {
+
+        const { username, password } = req.body;
+        if (username === process.env.ADMIN && password === process.env.ADMINPASSWORD) {
+            const payload = {
+                "id": username,
+                "role": "Admin"
+            }
+            const token = JwtOperation.generateToken(payload);
+
+            res.status(201).json({
+                token: token
+            });
+        } else {
+            res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+    };
+
+
+    static userRegister = async (req, res) => {
+        try {
+            const { email, password, name, role, department, employeeId } = req.body;
+
+            // Check if the user already exists with the same email
+            const [existingUser] = await pool.query('SELECT * FROM User WHERE email = ?', [email]);
+            if (existingUser.length !== 0) {
+                return res.status(400).json({
+                    success: false,
+                    message: "User with the same email already exists",
+                });
+            }
+
+            // Hash the user's password before saving it
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Insert new user into the User table
+            const newUser = await pool.query(
+                "INSERT INTO User (email, password, name, role, department, employeeId) VALUES (?, ?, ?, ?, ?, ?)",
+                [email, hashedPassword, name, role, department, employeeId]
+            );
+
+            if (newUser.affectedRows === 1) {
+                return res.status(201).json({
+                    success: true,
+                    message: "User registered successfully",
+                    userId: newUser.insertId,  // Return the new user's ID
+                });
+            } else {
+                return res.status(500).json({
+                    success: false,
+                    message: "Failed to register user",
+                });
+            }
+
+        } catch (error) {
+            console.error("Error during user registration:", error);
+            return res.status(500).json({
+                success: false,
+                message: "Internal server error",
+            });
+        }
+    };
+
+    static userLogin = async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const [userRows] = await pool.query('SELECT * FROM User WHERE email = ?', [email]);
+            console.log(userRows)
+            if (userRows.length === 0) {
+                return res.status(404).json({
+                    success: false,
+                    message: "User not found",
+                });
+            }
+
+            const user = userRows[0];
+
+
+            if (user.password !== password) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Incorrect password",
+                });
+            } else {
+                const payload = {
+                    "id": user.id,
+                    "role": user.role
+                };
+                const token = JwtOperation.generateToken(payload);
+                return res.status(200).json({
+                    token: token,
+                    success: true,
+                    message: "User Found",
+                    user: user,
+                });
+            }
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({
+                success: false,
+                message: "Internal Server Error",
+            });
+        }
+    };
+
+
+
+    static updatePassword = async (req, res) => {
+        try {
+            const { email, password, } = req.body;
+            console.log(email, password)
+            if (!email || !password) {
+                return res.status(400).json({ error: 'Email and password are required.' });
+            }
+
+            let query = 'UPDATE User SET password = ? where email = ?';
+            let values = [password,email];
+
+            const result = await pool.query(query, values);
+
+            return res.status(200).json({
+                message: 'Password updated successfully'
+            });
+        } catch (err) {
+            console.error('Error updating password:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    };
+
+
+    static forgetPassword = async (req, res) => {
+        try {
+            const { email } = req.body;
+
+            if (!email) {
+                return res.status(400).json({ message: 'Email is required' });
+            }
+
+
+            const [userResult] = await pool.query('SELECT * FROM User WHERE email = ?', [email]);
+
+            if (userResult.length === 0) {
+                return res.status(404).json({ message: 'User with this email does not exist' });
+            }
+
+            // Generate OTP
+            const otp = generateOTP();
+            console.log(otp)
+
+            // Send OTP email
+            sendOTPMail(email, otp, 'Password Reset OTP');
+
+            // Respond with success message
+            return res.status(200).json({ message: 'OTP has been sent to your email', otp: otp, user: userResult[0] });
+        } catch (error) {
+            console.error('Error in forgetPassword:', error);
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+    };
+}
+
+
+
+export default AuthHandler;
---
+++ server/server.mjs
@@ -0,0 +1,60 @@
+import cors from 'cors';  // Import the cors module
+import dotenv from 'dotenv'; 
+import express from 'express';
+import AuthRouter from './Route/AuthRouter.mjs';
+import ConferanceRouter from './Route/ConferanceRouter.mjs';
+import JwtOperation from './Utils/jwtoken.mjs'
+dotenv.config();
+
+const app = express();
+
+
+// JSON Parsing Middleware
+app.use(express.json());
+
+
+app.use(cors({
+    // origin: "http://localhost:3000",
+    origin:"*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+// Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.use((req, res, next) => {
+
+    if (req.path.startsWith('/api/auth')) {
+        // Skip JWT verification for /api/auth routes
+        return next();
+    }
+
+    const token = req.headers['authorization'];
+    
+    if (!token) {
+        return res.status(403).send('A token is required for authentication');
+    }
+    try {
+        const decoded = JwtOperation.verifyToken(token);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).send('Invalid Token');
+    }
+    next();
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+
+app.use("/api/auth",AuthRouter)
+app.use("/api/conferance",ConferanceRouter)
+
+
+app.listen(5000, '0.0.0.0');
